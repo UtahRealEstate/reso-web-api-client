48b579d69e997d58542f0498f0fa5e351645bb23
Fred Larsen <fredish@gmail.com>
2014-04-24 07:53:05 -0600

    first commit

---
+++ config/dev.js
@@ -0,0 +1,10 @@
+module.exports = {
+    authorizationURL: 'https://reso.fredish.com:9999/dialog/authorize',
+    tokenURL: 'https://reso.fredish.com:9999/oauth/token',
+    userInfoURL: 'https://reso.fredish.com:9999/api/userinfo',
+    host :  "reso.fredish.com",
+    port : '9999',
+    callbackURL: "http://reso.fredish.com:8080/auth/appexample/callback",
+    clientId: 'abc123',
+    clientSecret: 'ssh-secret'
+};
---
+++ config/prod.js
@@ -0,0 +1,10 @@
+module.exports = {
+    authorizationURL: 'https://reso.utahrealestate.com/dialog/authorize',
+    tokenURL: 'https://reso.utahrealestate.com/oauth/token',
+    userInfoURL: 'https://reso.utahrealestate.com/api/userinfo',
+    host :  "reso.utahrealestate.com",
+    callbackURL: 'http://reso.wfrmls.com:8080/auth/appexample/callback',
+    clientId: 'abc123',
+    clientSecret: 'ssh-secret',
+    port : '443'
+};
